d541a8536e544436b17652994ed850c3ba2fb35f
betz <betzinbal@gmail.com>
2024-08-21 14:22:26 +0300

    initial commit

---
+++ Rest/MosadRest/MosadRest/Models/Target.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MosadRest.Models
+{
+    public class Target
+    {
+        [Required]
+        public required string Name { get; set; }
+        [Required]
+        public required string position { get; set; }
+        public required string photo_url { get; set; }
+        public (int x, int y) Waypoint { get; set; } = (-1, -1);
+        public Enum ststus { get; set; }
+    }
+}
+
